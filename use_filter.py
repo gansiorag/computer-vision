9cc8138d0cf6e7c99bad87ac3bdaffa8cd54e724
gansior-ag <gansior-ag@rosseti.digital>
2021-05-26 12:32:43 +0300

    how use filrers on example one pichte

--- use_filter.py
+++ use_filter.py
@@ -1,5 +1,5 @@
 """
-This module make
+This module shows you how various filters work when applied to an image.
 
 author Gansior A.G. gansior@gansior.ru
 """
@@ -11,15 +11,11 @@ import matplotlib.pyplot as plt
 
 """
 Фильтры
-
 Предварительное преобразование по заранее определенным ядрам свертки (обычно 3x3). Например, фильтр четкости:
-		
 -1 	-1 	-1
--1 	9 	-1
+-1 	 9 	-1
 -1 	-1 	-1
-
 Сумма всех коэффициентов в фильтре равна 1.
-
 """
 
 def show_image (image):
@@ -28,30 +24,28 @@ def show_image (image):
     plt.show()
 
 
-
 if __name__ == '__main__':
     image = cv2.imread("barcode.example.png")
     show_image(image)
     show_image(cv2.filter2D(image, -1, np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])))
-    """
-    Размытие изображения
 
-Обычный фильтр размытия:
-		
-1/9 	1/9 	1/9
-1/9 	1/9 	1/9
-1/9 	1/9 	1/9
-
-Фильтр Гаусса:
-		
-1/16 	2/16 	1/16
-2/16 	4/16 	2/16
-1/16 	2/16 	1/16
     """
+        Размытие изображения
+        Обычный фильтр размытия:
+        1/9 	1/9 	1/9
+        1/9 	1/9 	1/9
+        1/9 	1/9 	1/9
+        Фильтр Гаусса:
+        1/16 	2/16 	1/16
+        2/16 	4/16 	2/16
+        1/16 	2/16 	1/16
+    """
+
     show_image(cv2.filter2D(image, -1, np.ones((3, 3)) / 9))
     show_image(cv2.filter2D(image, -1, np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]]) / 16))
     show_image(cv2.GaussianBlur(image, (3, 3), 0))
     show_image(cv2.medianBlur(image, 3))
+
     """
     Наращивание и эрозия
         Морфологические изменения (не "маски"). Наращивание берет максимальный элемент из заданной формы (окрестности), эрозия - минимальный. Форма крест/круг:
@@ -63,8 +57,8 @@ if __name__ == '__main__':
         1 	1 	1
         1 	1 	1
          Открытие = наращивание + эрозия. Закрытие = эрозия + наращивание
-
     """
+
     kernel = np.ones((3, 3), np.uint8)
     show_image(cv2.erode(image, kernel, iterations=3))
     show_image(cv2.dilate(image, kernel, iterations=3))
